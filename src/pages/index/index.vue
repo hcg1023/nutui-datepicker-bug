3bc3a8e2c1c19081d855ed7c0319492b286ce66b
haochenguang <2293885211@qq.com>
2024-08-13 11:47:57 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <view>
+    <nut-button type="primary" @click="click">JDT 风格按钮</nut-button>
+    <nut-popup v-model:visible="show" closeable position="bottom" style="height: 50%;">
+      <nut-date-picker v-model="value"></nut-date-picker>
+    </nut-popup>
+  </view>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+const show = ref(false);
+const click = () => {
+  show.value = true;
+};
+
+const value = ref(new Date())
+</script>
